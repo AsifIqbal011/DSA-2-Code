5038b8960ce79f05f048f2c5f556672c1b2c7100
MD ASIF IQBAL <120092229+AsifIqbal011@users.noreply.github.com>
2024-11-08 17:44:21 +0600

    Update fiboDP.cpp

--- fiboDP.cpp
+++ fiboDP.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int fibo(int n,int memo[])
 {
     if(n==0 || n==1) return memo[n]= n;
+    if(memo[n]!=-1) return memo[n];
     else return memo[n]=fibo(n-1,memo)+fibo(n-2,memo);
 }
 
@@ -15,11 +16,6 @@ int memorization(int n)
         memo[i]=-1;
     }
     int ans=fibo(n,memo);
-
-  //  for(int i=0; i<=n; i++)
-  //  {cout<<memo[i]<<" ";}
-  //  cout<<endl;
-
     return ans;
 }
 
