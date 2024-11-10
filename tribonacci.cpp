6c81960d7d014dd9d4ee767a3673ada355f4e3c6
MD ASIF IQBAL <120092229+AsifIqbal011@users.noreply.github.com>
2024-11-10 21:50:29 +0600

    dp

---
+++ tribonacci.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n,int memo[])
+{
+    if(n==0 || n==1) return memo[n]= 0;
+    if(n==2) return memo[n]= 1;
+    if(memo[n]!=-1) return memo[n];
+    else return memo[n]=fibo(n-1,memo)+fibo(n-2,memo)+fibo(n-3,memo);
+}
+
+int memorization(int n)
+{
+    int memo[n+1];
+    for(int i=0; i<=n; i++)
+    {
+        memo[i]=-1;
+    }
+    int ans=fibo(n,memo);
+     for(int i=0; i<=n; i++)
+    {
+        cout<<memo[i]<<" ";
+    }
+    cout<<endl;
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<memorization(n);
+
+    return 0;
+}
+
